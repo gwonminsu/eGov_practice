1b3a7e7503a7011cf7f9d130cb425db993d116db
gwonminsu <kd0615@naver.com>
2025-04-09 18:24:13 +0900

    mapper인터페이스 방식 없애고 DAO방식으로 통일
    제목이 곧 내용

--- src/main/java/egovframework/practice/test/service/impl/TestServiceImpl.java
+++ src/main/java/egovframework/practice/test/service/impl/TestServiceImpl.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.Collections;
 import java.util.List;
 
 // Service 구현체
@@ -17,11 +16,8 @@ public class TestServiceImpl implements TestService {
     // 로거
     private static final Logger LOGGER = LoggerFactory.getLogger(TestServiceImpl.class);
 
-    private TestDAO testDAO;
     @Resource(name = "testDAO")
-    public void setTestDAO(TestDAO testDAO) {
-        this.testDAO = testDAO;
-    }
+    protected TestDAO testDAO;
 
     // 데이터 리스트 조회
     @Override
